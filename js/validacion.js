93cf5211ab6b1ec3eb720f067a992f89f3f8289c
juanfonseca09 <juan.fonseca1153@gmail.com>
2022-10-21 09:52:23 -0300

    lm

--- js/validacion.js
+++ js/validacion.js
@@ -1,27 +1,29 @@
-let forms = document.querySelectorAll('.needs-validation')
-
-Array.from(forms).forEach(form => {
-    form.addEventListener('submit', event => {
-      if (form.checkValidity()) {
-        event.preventDefault()
-      }
-      else if (!form.checkValidity()) {
-        event.preventDefault()
-        event.stopPropagation()
-      }
-
-      form.classList.add('was-validated')
-    }, false)
+(function () {
+    'use strict'
+    let forms = document.querySelectorAll('.needs-validation')
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
   })()
 
-  const contraseña1 = document.getElementById("password1").value;
-  const contraseña2 = document.getElementById("password2").value;
-  const feedback = document.getElementById("feedback");
+  const contraseña1 = document.getElementById("password1");
+  const contraseña2 = document.getElementById("password2");
+  const btn = document.getElementById("btn");
 
-  const password = () => {
-    if(contraseña1==contraseña2){
-        console.log(contraseña1)
-        feedback.classList.add('was-validated');
+btn.addEventListener('click', () => {
+    if(contraseña1.value==contraseña2.value){
+        
+        contraseña2.classList.add('is-valid');
+    } else {
+        contraseña2.classList.add('is-invalid');
     }
   } 
-
+)
